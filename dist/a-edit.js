e1d75db5b1fb7665bf199e4f1462834d5652e4ee
Jonybang <jonybang@mail.ru>
2016-06-15 11:19:00 +1000

    delete select-adder

--- dist/a-edit.js
+++ dist/a-edit.js
@@ -993,21 +993,6 @@ angular
         };
     }]);
 
-//angular.module('a-edit').directive('selectAdder', [function() {
-//    return {
-//        restrict: 'C',
-//        link: function ($scope, $element, $attrs){
-//            $element.click(function($event){
-//                var $element = angular.element($event.target);
-//                if(!$element.hasClass('btn'))
-//                    $event.stopPropagation();
-//
-//                if($element.hasClass('btn-default'))
-//                    $event.stopPropagation();
-//            });
-//        }
-//    };
-//}]);
 /**
  * Created by jonybang on 04.07.15.
  */
